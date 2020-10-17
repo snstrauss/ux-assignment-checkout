a503f16c017c81f9f4224a4e91e66c477ea880d2
snstrauss <zahavi.guy.85@gmail.com>
2020-10-17 10:31:47 +0300

    html rootElement will now get window.innerHeight as it's height, so the app will take all AVAILABLE space on mobile, and not screen height.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 
 import Checkout from './Checkout';
 
+document.documentElement.style = `height: ${window.innerHeight}px`;
+
 ReactDOM.render(<Checkout />, document.getElementById('root'));
